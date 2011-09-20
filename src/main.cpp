d07d26ca4246616af30113cfea289ea3a01ae6ad
Dan Tumaykin <d.tumaykin@gmail.com>
2011-09-20 15:07:40 +0200

    Aggiunte strutture dei dati.
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-// Written by Dan Tumaykin
 #include <iostream>
+#include "structures.h"
 
 void main()
 {
-	std::cout << "hello, world!";
+	scene_t scene;
+
+	// config - una funzione che da un file di configurazione prende i valori e ritorna una scena completa
+
+	// render - trasforma scena completa in un array di pixel
+
+	// output - array di pixel diventa un immagine
+
 }
---
+++ src/structures.h
@@ -0,0 +1,77 @@
+// strutture dati utilizzate da programma
+
+// basic structures
+struct point_t {
+	double x, y, z;
+};
+
+struct vector_t {
+	double x, y, z;
+};
+
+struct color_t {
+	double r, g, b;
+};
+
+struct pixel_t {
+	double r, g, b;
+	int i, j;
+};
+
+// primitives
+enum PRIMITIVES
+{
+	SPHERE,
+	POLIGON,
+	CONSTRUCTOR
+};
+
+struct sphere_t {
+	point_t center;
+	double r;
+};
+
+struct poligon_t {
+	point_t ptA, ptB, ptC; // poligon is defined by 3 points
+};
+
+struct prim_t {
+	int type; //type from PRIMITIVES
+	int mat; //material id
+
+	union prim { //union with primitives
+		sphere_t sphere;
+		poligon_t poligon;
+	};
+};
+
+struct light_t {
+	point_t x, y, z;
+};
+
+struct material_t {
+	color_t col;
+	double coefDiffuse;
+	double coefReflect;
+	double coefRefract;
+};
+
+
+
+struct scene_t {
+
+	int screenResX;
+	int screenResY;
+	double screenSizeX;
+	double screemSizeY;
+
+	//point_t cam; //camera position, now unused
+
+	
+	//scene content
+	int primCount, lightCount, matCount;
+	prim_t *prim;
+	light_t *light;
+	material_t *material;
+
+};
