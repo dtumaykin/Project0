8602539bfa68a2a01a5effacd9342486a2606c7f
Dan Tumaykin <d.tumaykin@gmail.com>
2011-09-20 17:09:04 +0200

    Aggiunto esempio di overloading di operatore << di std::ostream.
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- docs/sample/operator.cpp
+++ docs/sample/operator.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+
 struct Point
 {
-	char x, y, z, w;
+	double x, y, z, w;
 };
 
 Point operator+ (Point &p1, Point &p2)
 {
 	Point p = {p1.x + p2.x, p1.y + p2.y, p1.z + p2.z, p1.w + p2.w};
 	return p;
+}
+
+std::ostream& operator<< (std::ostream &out, Point &p)
+{
+	out << "(x : " << p.x << ", y: " << p.y << ", z: " << p.z << ", w: " << p.w << ")";
+	return out;
 }
 
 int main()
@@ -17,7 +24,7 @@ Point p1 = {65, 67, 64, 60};
 Point p2 = {2, 6, 1, 19};
 
      Point p3 = p1+p2;
-     std::cout << p3.x << " " << p3.y << " " << p3.z << " " << p3.w;
+     std::cout << p3;
      std::cin.get();
 
 return 0;
