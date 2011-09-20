9785e145223ce94a6cef2df2dd0f77cfcb79223a
Dan Tumaykin <d.tumaykin@gmail.com>
2011-09-20 19:24:44 +0200

    testing newly added functions
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "structures.h"
+#include "util.h"
 
 void main()
 {
 	scene_t scene;
+
+	vector_t vect = { 1, 2, 3};
+	std::cout << vect << "\n";
+	std::cout << length(vect) << "\n";
+	std::cout << dot(vect, vect) << "\n";
+	std::cout << norm(vect);
+
+	std::cin.get();
+
 
 	// config - una funzione che da un file di configurazione prende i valori e ritorna una scena completa
 
