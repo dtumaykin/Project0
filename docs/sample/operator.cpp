80f22ddfcb19e84be46fc2fdb3eda281e7002ff6
Furai93 <furai93@gmail.com>
2011-09-20 16:05:42 +0300

    Edited docs/sample/operator.cpp via GitHub

--- docs/sample/operator.cpp
+++ docs/sample/operator.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 struct Point
 {
-	double x, y, z;
+	char x, y, z,w;
 };
 
 Point operator+ (Point &p1, Point &p2)
 {
-	Point p = {p1.x + p2.x, p1.y + p2.y, p1.z + p2.z};
+	Point p = {p1.x + p2.x, p1.y + p2.y, p1.z + p2.z, p1.w+p2.w};
 	return p;
 }
 
 int main()
 {
-Point p1 = {1, 2, 3};
-Point p2 = {4, 5, 6};
+Point p1 = {65, 67, 64, 60};
+Point p2 = {2, 6, 1, 19 };
 
 Point p3 = p1+p2;
 
-std::cout << p3.x << " " << p3.y << " "<< p3.z;
+std::cout << p3.x << " " << p3.y << " "<< p3.z <<" "<< p3.w;
 std::cin.get();
 
 return 0;
