29cf9ca97d0c9e696651b42a51a13234b50f22d9
Dan Tumaykin <d.tumaykin@gmail.com>
2011-09-24 18:01:39 +0200

    disable warnings for fscanf(), fopen()
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
+
 #include "structures.h"
 #include "util.h"
 #include "config.h"
 
+#define _CRT_SECURE_NO_WARNINGS // no warnings for fscanf(), fopen()
+
 void main()
 {
-	scene_t scene;
-
-	vector_t vect = { 1, 2, 3};
-	std::cout << vect << "\n";
-	std::cout << length(vect) << "\n";
-	std::cout << dot(vect, vect) << "\n";
-	std::cout << norm(vect);
-
-	std::cin.get();
-
 
 	// config - una funzione che da un file di configurazione prende i valori e ritorna una scena completa
 
