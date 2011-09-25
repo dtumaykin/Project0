60d16dda03280692b781cdcf65434f875b573696
Dan Tumaykin <d.tumaykin@gmail.com>
2011-09-25 20:12:41 +0200

    added enum with opcodes - need it for parser later
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- src/config.h
+++ src/config.h
@@ -1,6 +1,17 @@
 #ifndef P0_CONFIG_H
 #define P0_CONFIG_H
 
+
+enum OPCODES 
+{
+	RESO,
+	SIZE,
+	SPHR,
+	POLI,
+	LGHT,
+	MATR
+};
+
 int loadConfig(char * path, scene_t &scene); //return 0 in case of bad config
 
 scene_t config(); // loads default "config.cfg" file and returns a scene, implemented by secco92
