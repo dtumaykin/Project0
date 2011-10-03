c7d3d0ec09b892ca0c0041b9a0feb6f9634a3154
Dan Tumaykin <d.tumaykin@gmail.com>
2011-10-03 22:04:27 +0200

    material "by id" search
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -19,13 +19,13 @@ bool getIntersection(prim_t &p, ray_t &r, double &t)
 		B = r.dst * PoPc; 
 		D = B*B - PoPc*PoPc + p.sphere.r*p.sphere.r; 
 
-		if(D<0)
+		if(D < 0.0f)
 			return 0;
 
 		x1 = B + sqrt(D);
 		x2 = B - sqrt(D);
 
-		if(x1<x2)
+		if(x1 < x2)
 		{
 			if(t > x1) 
 			{
@@ -61,8 +61,9 @@ bool getIntersection(prim_t &p, ray_t &r, double &t)
 color_t getColor(int x, int y, scene_t &scene)
 {
 	color_t c = { 0, 0, 0};
-	prim_t p;
-	material_t m;
+	int currPrimitive = -1;
+	prim_t *p;
+	material_t *m;
 	double t = 2000.0f;
 
 	double pixelSizeX = scene.screenSizeX/scene.screenResX;
@@ -71,13 +72,21 @@ color_t getColor(int x, int y, scene_t &scene)
 	ray_t ray = { {x * pixelSizeX, y * pixelSizeY, -1000.0f}, {0.0f, 0.0f, 1.0f}};
 	norm(ray.dst);
 
+	//get nearest intersection
 	for(int i = 0; i < scene.primCount; i++)
 		if(getIntersection(scene.prim[i], ray, t))
-		{
-			p = scene.prim[i];
-			m = scene.material[p.mat - 1];
-			c = m.col;
-		}
+			currPrimitive = i;
+
+	//test if there are intersections
+	if(currPrimitive == -1) return c;
+		p = &scene.prim[currPrimitive];
+
+	//find material for primitive
+	for(int i = 0; i < scene.matCount; i++)
+		if(scene.material[i].mId == p->mat)
+			m = &scene.material[i];
 	
+	if(m) c = m->col;
+
 	return c;
 }
